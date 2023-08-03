c8cdbaa5f0f0e307c4df325712fa03c4dae30438
Henry Oo <oohenry225@gmail.com>
2023-08-03 16:31:08 +0630

    add form handling

--- public/views/dashboard.php
+++ public/views/dashboard.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Admin Dashboard</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/x-icon" href="./assets/image/cufavicon.ico">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="./assets/css/styles.css">
     </head>
     <body>
         <h1>Dashboard</h1>
--- src/form_handler.php
+++ src/form_handler.php
@@ -1,17 +1,7 @@
  <?php
 
     require_once '../config.php';
-
-    // form data
-    $instructorName = $_POST["instructorName"];
-    $academicYear   = $_POST["academicYear"];
-    $classroom      = $_POST["classroom"];
-    $semester       = $_POST["semester"];
-
-    for($i = 0; $i <= 15; $i += 1) {
-        $arr[$i] = $_POST[$i+1];
-    }
-
+    
     // variables for db connection
     $servername  = DB_HOST;
     $database    = DB_NAME;
@@ -25,13 +15,98 @@
         die("Connection Failed");
     }
 
-    // processes form data
-    $instructorName = $conn->real_escape_string($instructorName);
-    $academicYear   = $conn->real_escape_string($academicYear);
-    $classroom      = $conn->real_escape_string($classroom);
+    // form data
+    $instructor     = $_POST["instructorName"];
+    $academicYear   = $_POST["academicYear"];
+    $semester       = $_POST["semester"];
+    $course         = $_POST["course$semester"];
 
-    //prepare and bind
-    $prepare = $conn->prepare("");
+    // question count 
+    $count = 16;
+ 
+    // 16 questions
+    for($i = 0; $i < $count; $i += 1) {
+        $arr[$i] = $_POST[$i+1];
+    }
+
+    // processes form data
+    $instructor     = $conn->real_escape_string($instructor);
+    $academicYear   = $conn->real_escape_string($academicYear);
+    $course         = $conn->real_escape_string($course);
+
+    // prepare - insert statement
+    $insertTeaches = $conn->prepare("INSERT INTO teaches (instructor,academicYear,course) VALUES (?,?,?)");
+    $insertPoint   = $conn->prepare("INSERT INTO point (teaches, question) VALUES (?,?)");
+
+    // prepare and bind and execute - select statement
+    $selectTeaches = $conn->prepare("SELECT teaches.ID as teaches_id, instructor, academicYear, course, course.ID as course_id, name, semester FROM teaches JOIN course ON teaches.course = course.ID WHERE instructor = ? AND academicYear = ? and course.ID = ?");
+    $selectTeaches->bind_param("sss", $instructor, $academicYear, $course);
+    $selectTeaches->execute();
+    $resultPoint = $selectTeaches->get_result();
+
+    // teaches row ID 
+    $teachesID = null;
+    
+    // if row for teaches doesn't exist
+    if($resultPoint->num_rows == 0){
+        $insertTeaches->bind_param("sss", $instructor, $academicYear, $course);
+        $insertTeaches->execute();
+    }
+
+    // to get the ID column of teaches
+    $selectTeaches->execute();
+    $resultPoint = $selectTeaches->get_result();
+    $row = $resultPoint->fetch_assoc();
+    $teachesID = $row["teaches_id"];
+    
+    // if point rows for that teachesID doesn't exist, create them
+    $selectPoint = $conn->prepare("SELECT * FROM point WHERE teaches = ?");
+    $selectPoint->bind_param("i",$teachesID);
+    $selectPoint->execute();
+    $resultPoint = $selectPoint->get_result();
+
+    if($resultPoint->num_rows == 0){
+        for($i=1; $i<=$count; $i+=1){
+            $insertPoint->bind_param("ii", $teachesID, $i);
+            $insertPoint->execute();
+        }
+    }
+    
+    // update them 
+    $updateTotallyDisagree = $conn->prepare("UPDATE point SET totally_disagree = totally_disagree + 1 WHERE teaches = ? AND question = ?");
+    $updateDisagree = $conn->prepare("UPDATE point SET disagree = disagree + 1 WHERE teaches = ? AND question = ?");
+    $updateNeutral = $conn->prepare("UPDATE point SET neutral = neutral + 1 WHERE teaches = ? AND question = ?");
+    $updateAgree = $conn->prepare("UPDATE point SET agree = agree + 1 WHERE teaches = ? AND question = ?");
+    $updateTotallyAgree = $conn->prepare("UPDATE point SET totally_agree = totally_agree + 1 WHERE teaches = ? AND question = ?");
+
+    for ($i=1; $i<=$count; $i+=1) {
+        switch ($arr[$i-1]) {
+            case 1:
+                $updateTotallyDisagree->bind_param("ii", $teachesID, $i);
+                $updateTotallyDisagree->execute();
+                break;
+
+            case 2:
+                $updateDisagree->bind_param("ii", $teachesID, $i);
+                $updateDisagree->execute();
+                break;
+
+            case 3:
+                $updateNeutral->bind_param("ii", $teachesID, $i);
+                $updateNeutral->execute();
+                break;
+
+            case 4:
+                $updateAgree->bind_param("ii", $teachesID, $i);
+                $updateAgree->execute();
+                break;
+
+            case 5:
+                $updateTotallyAgree->bind_param("ii", $teachesID, $i);
+                $updateTotallyAgree->execute();
+                break;
+        }
+    }
     
     // close connection
     $conn->close();
