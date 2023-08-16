ab7edfd701b1ab42c5c846fe352029b0b62ea548
Henry Oo <oohenry225@gmail.com>
2023-08-16 07:18:40 +0630

    improves dashboard

--- public/views/dashboard.php
+++ public/views/dashboard.php
@@ -9,19 +9,51 @@
         <link rel="icon" type="image/x-icon" href="./assets/image/cufavicon.ico">
         <link rel="stylesheet" href="./assets/css/styles.css">
         <link rel="stylesheet" href="./assets/css/dashboard.css">
+        <!-- icons -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     </head>
     <body class="school-bg">
-        <div class="menu-box">
-            <div class="menu">
-                <a href="/admin/instructor" style="background-color:#c2e1c2;">Manage Instructor</a>
-                <a href="/admin/course" style="background-color:#e2d7f1;">Manage Course</a>
-                <a href="/admin/search" style="background-color:#f4b6c2;">Search Records</a>
-                <a href="/admin/update" style="background-color:#a0c8ff;">Update Records</a>
-            </div>
-            <form action="./src/login_handler.php" method="POST" class="flex row">
-                <button type="submit" name="log_button" value="logout" onclick="return showConfirmation('Are you sure you want to log out ?')"> Log Out </button>
-            </form>
+        <div class="menu-wrapper">
+            <a class="menu-item" href="/admin/search">
+                <div class="icon flex col">
+                    <span class="material-icons"> search </span>
+                </div>
+                <div class="title flex col">
+                    Search Records 
+                </div>
+            </a>
+            <a class="menu-item" href="/admin/search">
+                <div class="icon flex col">
+                    <span class="material-icons"> storage </span>
+                </div>
+                <div class="title flex col">
+                    Update Records 
+                </div>
+            </a>
+            <a class="menu-item" href="/admin/search">
+                <div class="icon flex col">
+                    <span class="material-icons"> people </span>
+                </div>
+                <div class="title flex col">
+                    Manage Instructors
+                </div>
+            </a>
+            <a class="menu-item" href="/admin/search">
+                <div class="icon flex col">
+                <span class="material-icons"> delete </span>
+                </div>
+                <div class="title flex col">
+                    Bulk Deletion
+                </div>
+            </a>
         </div>
+        <form action="./src/login_handler.php" method="POST" class="btn-wrapper flex row">
+            <button class="btn" type="submit" name="log_button" value="logout" onclick="return showConfirmation('Are you sure you want to log out ?')"> Log Out </button>
+        </form>
     </body>
     <script src="./assets/javascript/submit-confirmation.js"> </script>
 </html>
